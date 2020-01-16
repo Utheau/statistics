3ec1b112ed7d82aee22ae751b805ed284b83d854
Antonio Paolo Mecci <antoniopaolomecci@gmail.com>
2020-01-16 17:21:54 +0100

    refactor the code based on composition (v2)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
+//import { MatchReader } from './inheritance/MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
+const csvFileReader = new CsvFileReader('football.csv');
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUniWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUniWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
