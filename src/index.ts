447a5db227bea69c35c3fb3e32d2959ff18472cc
Antonio Paolo Mecci <antoniopaolomecci@gmail.com>
2020-01-16 15:23:36 +0100

    initial setup and implementation of the app's logic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import fs from 'fs';
+import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-})
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+let manUniWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUniWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUniWins++;
   }
-  );
-console.log(matches)
+}
+
+console.log(`Man United won ${manUniWins} games`);
